55bcc0278491ae76f5e114cd8a13aede30181dcb
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2020-08-01 21:49:20 -0400

    Update nlpMethods.py

--- nlpMethods.py
+++ nlpMethods.py
@@ -2,7 +2,7 @@ import nltk
 from textblob import TextBlob, Word
 import pycountry
 
-
+# Returns a list of spelling mistakes the user made and suggestions to correct the word
 def spellingCheck(text):
 	splits = text.split()
 	corrections = ""
@@ -19,7 +19,7 @@ def spellingCheck(text):
 		corrections = "No spelling mistakes!"
 	return corrections
 
-
+# Detects the language of a phrase and translates it into English.
 def translatePhrase(text):
 	b = TextBlob(text)
 	iso = b.detect_language()
